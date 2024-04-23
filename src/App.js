65c43e92e6e60ea82ddad349fd112728b1de4138
Zaynab Azzouzi <zazzouzi@Zaynabs-MacBook-Pro.local>
2024-04-23 21:30:34 +0200

    basics for welcome and name screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from './screens/WelcomeScreen';
+import NameScreen from './screens/NameScreen';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const Stack = createStackNavigator();
+
+
+export default function App() {
+ return (
+   <NavigationContainer>
+     <Stack.Navigator initialRouteName="Welcome">
+       <Stack.Screen name="Welcome" component={WelcomeScreen} />
+       <Stack.Screen name="Name" component={NameScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
+};
+
